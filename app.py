13ebedea1ae9adfdf131ff12f2a8ca510b578cb2
KUM JUDE <kumjude09@gmail.com>
2023-05-22 17:27:57 +0100

    created register, login, homepage, and dashboard templates

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
     """ Returns application's Hompage"""
-    return "Homepage"
+    return render_template('index.html')
 
 @app.route('/register', methods=['GET'])
 def register_form():
 
     """ Displays applications's registration form """
 
-    return "Registration Form"
+    render_template('register.html')
 
 @app.route('/login', methods=['GET'])
 def login_form():
 
     """ Displays applications's registration form """
 
-    return "Login Form"
+    render_template('login.html')
 
 
 @app.route('/dashboard', methods=['GET'])
@@ -27,7 +27,7 @@ def dashboard():
 
     """ Displays user's dashboard """
 
-    return "Dashboard"
+    render_template('dashboard.html')
 
 if __name__ == "__main__":
 
