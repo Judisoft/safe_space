92c956998881dd73097836c579628e800477b0a3
KUM JUDE <kumjude09@gmail.com>
2023-05-30 00:01:16 +0100

    User authentication completed

--- safe_space/routes.py
+++ safe_space/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, url_for, jsonify, flash, redirect
+from flask import render_template, request, url_for, jsonify, flash, redirect, request
 from safe_space import app
 from safe_space.forms import RegistrationForm, LoginForm
 from safe_space.models import User 
@@ -6,7 +6,7 @@ from safe_space.fetch_api_data import get_mod_data
 from safe_space.get_url_data import url_data
 from flask_bcrypt import Bcrypt
 from safe_space.models import db
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 bcrypt = Bcrypt(app)
@@ -32,7 +32,8 @@ def index():
 def register():
 
     """ Handles user registration """
-
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -48,13 +49,15 @@ def register():
 def login():
 
     """ Handles user login """
-
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            return redirect(url_for('dashboard'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash('Login Unsuccessful. Please check your email and password', 'danger')
            
@@ -62,11 +65,23 @@ def login():
 
 
 @app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
 def dashboard():
 
     """ Displays user's dashboard """
+
     # text = url_data('param') # param is the API endpoint 
 
     # mod_report = get_mod_data(text)
 
     return render_template('dashboard.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return "User Profile"
